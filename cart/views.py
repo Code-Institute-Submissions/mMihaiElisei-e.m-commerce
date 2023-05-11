dbeb6e9907278bd9a54d4583e0ec2ab355bdaf3b
MihaiElisei <>
2023-05-11 19:44:49 +0000

    added remove items from cart and decrese items quantity from cart functionality

--- cart/views.py
+++ cart/views.py
@@ -102,3 +102,38 @@ def add_to_cart(request, product_id):
         cart_item.save()
     
     return redirect('view_cart')
+
+
+def remove_from_cart(request, product_id, cart_item_id):
+    """ A view to decrese items number from the cart """
+
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        cart_item = CartItem.objects.get(
+            product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+            messages.success(
+                request, f'Removed {product.name} from your cart!')
+
+        else:
+            cart_item.delete()
+            messages.success(
+                request, f'Removed {product.name} from your cart!')
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e} from your cart!')
+    return redirect('view_cart')
+
+
+def remove_cart_item(request, product_id, cart_item_id):
+    """ A view to remove items from the cart """
+
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(
+        product=product, cart=cart, id=cart_item_id)
+    cart_item.delete()
+    messages.success(request, f'Removed {product.name} from your cart!')
+    return redirect('view_cart')
