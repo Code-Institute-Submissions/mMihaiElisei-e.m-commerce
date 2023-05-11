450bb33379c17054897b35aa5545fab15bb3c6c7
MihaiElisei <>
2023-05-11 18:56:57 +0000

    added product detail

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<slug:category_slug>/', views.all_products,
+         name='products_by_category'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
