46e5a2d5d1f1b3234d9c1eaa1bdf3661fa8eb506
Mihai <mihai.m.elisei@gmail.com>
2023-05-13 18:34:06 +0100

    sending real emails 02

--- em_com/settings.py
+++ em_com/settings.py
@@ -214,6 +214,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'emcommerce@exemple.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
